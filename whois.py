e7d67f2210e5e5d1f1da7eb5753a7a6337fff3be
Viodex <viodex02@gmail.com>
2026-01-05 22:01:58 +0200

    Update whois.py

--- whois.py
+++ whois.py
@@ -1,7 +1,7 @@
 async def run(ctx):
     if ctx.url == "https://google.com":
-        ctx.log("this is url rilative to google")
+        ctx.print("this is url rilative to google")
     if "google" in ctx.response.text:
-        ctx.log("googled found")
+        ctx.print("googled found")
     else:
         ctx.log("pass")
