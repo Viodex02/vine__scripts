402b5c224fa60753ab4039ba47fd24a461f88369
Viodex <viodex02@gmail.com>
2026-01-05 21:02:18 +0200

    Add whois lookup function for domain information
    this script will be used via vine --GitRaw "RAW-URL-HERE"
    
    warning !! :
    this codes are safe as the publisher is Viodex & VIne team them selves .. any other source you use to use the RawGit in Vine you should read the source code first .. as it may be have a malware , reverse shell or any harmful piece of code 
    
    we will try to protect you inside VIne but don't forget that using a scanner to find a logic flow is impossible ...

---
+++ whois.py
@@ -0,0 +1,12 @@
+import whois
+
+def whoisThis(domain):
+    domain = domain.replace("http://", "")
+    domain = domain.replace("https://", "")
+    domain = domain.strip("/")
+
+    try:
+        whois_info = whois.whois(domain)
+        print(f"{whois_info}\n")
+    except Exception as e:
+        print(e)
