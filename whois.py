b61b6fb30703900af6d592433e48a6b641813f73
Viodex <viodex02@gmail.com>
2026-01-06 02:05:42 +0200

    Update whois.py

--- whois.py
+++ whois.py
@@ -1,8 +1,8 @@
 async def run(ctx):
-    if "google.com" in ctx.url:
-        ctx.Println("this url is related to google")
-
-    if "google" in ctx.response.text.lower():
-        ctx.Println("google found")
+    if ctx.url == "https://google.com":
+        print("found a googleSite")
     else:
-        ctx.Println("pass")
+        print("some URLs aren't googled here")
+
+    if "google" in ctx.response.text:
+        print(f"found some regex")
