629fb230ff9f82f12e3c9b40c4ff7325ed713309
Viodex <viodex02@gmail.com>
2026-01-06 14:29:53 +0200

    Update test2.py

--- test2.py
+++ test2.py
@@ -3,7 +3,7 @@ async def run(ctx):
   code = ctx.response.status_code
   if code == 200:
     print(f"[+]{Fore.GREEN} found a {code} code")
-  elif code = 302:
+  elif code == 302:
     print(f"[+]{Fore.YELLOW} found a {code} code")
   else:
     print(f"{Fore.RED}unknow code")
