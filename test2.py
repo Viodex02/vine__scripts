905818f659ea5b60465a6c6e81c4639598d4fdd4
Viodex <viodex02@gmail.com>
2026-01-11 22:20:13 +0200

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,6 +1,6 @@
 import httpx
 async def run(ctx):
-    url = int(input("enter the url"))
+    url = int(input("enter the url : "))
     for i in range(0,100):
         resp = httpx.get(url)
         print(resp.status_code)
